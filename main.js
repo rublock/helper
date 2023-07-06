39665e2784f3a4735c3665c2286e188d2cf88498
rublock <hypermail@yandex.ru>
2023-07-06 12:59:11 +0300

    JavaScript

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
-const data = 'text_data'
+//базовый промис
+const p = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('Preparing data...')
+        const data = '1123581321'
+        resolve(data)
+    }, 2000)
+})
 
-const dictData = {
-    data: 'text_dictData',
-    consoleLogInfo: function() {
-        console.log(this.data) //text_dictData
-        console.log(data) //text_data
-    }
-}
+// этот коллбэк будет вызван только после выполнения resolve()
+p.then(data => {
+    console.log(`Data preparied ${data}`) //Data preparied 1123581321
+})
 
-dictData.consoleLogInfo();
+//если нужно вызвать еще один промис, без глубокой вложенности
+//удобно, что объект p можно передавать между функциями, модулями..
+p.then(data => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Modifying data...')
+            data += ' fibonachi'
+            resolve(data) //или если ошибка reject()
+        }, 2000)
+    })
+}).then(data => {
+    console.log(`Modified data ${data}`)//Modified data 1123581321 fibonachi
+    return data //можно делать манипуляции с данными и возвращать их
+}).catch(err => console.log('Error', err)) //обработка ошибки если reject()
+  .finally(() => console.log('Finally')) //окончание скрипта
